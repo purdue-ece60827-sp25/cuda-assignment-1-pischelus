2f5802e811256090ef7d625c98dcd9da6a12e3ba
Kevin <kdarmasa@purdue.edu>
2025-01-28 18:28:42 -0500

    Part 2 - Monte Carlo is DONE.

--- src/cudaLib.cu
+++ src/cudaLib.cu
@@ -18,7 +18,7 @@ void saxpy_gpu (float* x, float* y, float scale, int size) {
 
 	// Ensure it's within the bounds
 	if (idx < size)
-		y[idx] = scale * x[idx] + y[idx];
+		y[idx] += scale * x[idx];
 }
 
 int runGpuSaxpy(int vectorSize) {
@@ -26,13 +26,15 @@ int runGpuSaxpy(int vectorSize) {
 
 	std::cout << "Hello GPU Saxpy!\n";
 
-	float * x, * y, * z; // z is for verification
-	float * x_d, *y_d; // device vectors
+	float * x, * y, * z;
+	float * x_d, *y_d;
 	float scale = 2.0f;
 
 	// Allocate hosts
 	x = (float*)malloc(vectorBytes);
 	y = (float*)malloc(vectorBytes);
+
+	// Z is for verification
 	z = (float*)malloc(vectorBytes);
 
 	if (x == NULL || y == NULL || z == NULL) {
@@ -69,7 +71,7 @@ int runGpuSaxpy(int vectorSize) {
 
 	// Kernel setup
 	int threadsPerBlock = 256;
-	int numBlocks = (vectorSize + threadsPerBlock - 1) / threadsPerBlock;
+	int numBlocks = ceil(vectorSize/256.0);
 
 	// Kernel invocation code
 	saxpy_gpu<<<numBlocks, threadsPerBlock>>>(x_d, y_d, scale, vectorSize);
@@ -96,7 +98,7 @@ int runGpuSaxpy(int vectorSize) {
 	free(y);
 	free(z);
 
-	// Dellocate devices
+	// Deallocate devices
 	gpuErrchk(cudaFree(x_d));
 	gpuErrchk(cudaFree(y_d));
 
@@ -117,12 +119,59 @@ int runGpuSaxpy(int vectorSize) {
 
 __global__
 void generatePoints (uint64_t * pSums, uint64_t pSumSize, uint64_t sampleSize) {
-	//	Insert code here
+	uint64_t hitCount = 0;
+
+	// Calculate thread index
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+	// Don't execute thread if index > expected size
+	if (idx >= pSumSize) {
+		return;
+	}
+
+	// Setup RNG
+	curandState_t rng;
+	curand_init(clock64(), idx, 0, &rng);
+
+	// Main GPU Monte-Carlo Code - Get random values
+	for (int i = 0; i < sampleSize; i++) {
+		float x = curand_uniform(&rng);
+		float y = curand_uniform(&rng);
+
+		// If x^2 + y^2 <= 1.0f, increment hitCount
+		if ((x * x + y * y) <= 1.0f) {
+			++hitCount;
+		}
+	}
+
+	// Store hitCount result in each thread in pSums array
+	pSums[idx] = hitCount;
+	
 }
 
 __global__ 
 void reduceCounts (uint64_t * pSums, uint64_t * totals, uint64_t pSumSize, uint64_t reduceSize) {
-	//	Insert code here
+	uint64_t pSum = 0;
+
+	// Calculate thread index
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+	// If pSumSize isn't evenly divisible by reduceSize
+	if (idx >= (pSumSize + reduceSize - 1) / reduceSize) {
+		return;
+	}
+
+	uint64_t reduceIdx = idx * reduceSize;
+	
+	// Accumulate into pSum
+	for (uint64_t i = 0; i < reduceSize; ++i) {
+		if (reduceIdx + i < pSumSize) {
+			pSum += pSums[reduceIdx + i];
+		}
+	}
+
+	// Store pSum result in each thread in totals array
+	totals[idx] = pSum;
 }
 
 int runGpuMCPi (uint64_t generateThreadCount, uint64_t sampleSize, 
@@ -156,8 +205,46 @@ double estimatePi(uint64_t generateThreadCount, uint64_t sampleSize,
 	
 	double approxPi = 0;
 
-	//      Insert code here
-	std::cout << "Sneaky, you are ...\n";
-	std::cout << "Compute pi, you must!\n";
+	uint64_t * totals;
+	uint64_t * hits_d, * totals_d;
+	uint64_t totalHitCount = 0;
+	uint64_t vectorBytes = generateThreadCount * sizeof(uint64_t);
+	uint64_t reduceBytes = reduceThreadCount * sizeof(uint64_t);
+
+	// Allocate hosts
+	totals = (uint64_t *)malloc(reduceBytes);
+
+	if (totals == NULL) {
+		printf("Unable to malloc memory ... Exiting!");
+		return -1;
+	}
+
+	// Allocate devices
+	gpuErrchk(cudaMalloc(&hits_d, vectorBytes));
+	gpuErrchk(cudaMalloc(&totals_d, reduceBytes));
+
+	// Kernel setup
+	int threadsPerBlock = 256;
+	int numBlocks = ceil(generateThreadCount/256.0);
+
+	// Kernel invocation code
+	generatePoints<<<numBlocks, threadsPerBlock>>>(hits_d, generateThreadCount, sampleSize);
+	gpuErrchk(cudaPeekAtLastError());
+	gpuErrchk(cudaDeviceSynchronize());
+	reduceCounts<<<numBlocks, threadsPerBlock>>>(hits_d, totals_d, generateThreadCount, reduceSize);
+	gpuErrchk(cudaPeekAtLastError());
+	gpuErrchk(cudaDeviceSynchronize());
+
+	// Kernel computation is done, copy device to host
+	gpuErrchk(cudaMemcpy(totals, totals_d, reduceBytes, cudaMemcpyDeviceToHost));
+
+	// Accumulate all totals from different threads
+	for (int i = 0; i < reduceThreadCount; ++i) {
+		totalHitCount += totals[i];
+	}
+
+	approxPi = ((double)totalHitCount / sampleSize) / generateThreadCount;
+	approxPi = approxPi * 4.0f;
+
 	return approxPi;
 }
